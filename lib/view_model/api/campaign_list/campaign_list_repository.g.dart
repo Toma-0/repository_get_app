33fe4a1bf567f144d2c6ae80372fa28927109a4b
Toma-0 <toma0122yukako@icloud.com>
2024-04-16 23:22:15 +0900

    データを取得するクラスのみ作成

---
+++ lib/view_model/api/campaign_list/campaign_list_repository.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'campaign_list_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$fetchRepositoryListHash() =>
+    r'8a34c1560e4070a647bdc2f129ac6bf712183bc8';
+
+/// See also [fetchRepositoryList].
+@ProviderFor(fetchRepositoryList)
+final fetchRepositoryListProvider =
+    AutoDisposeFutureProvider<List<RepositoryListState>>.internal(
+  fetchRepositoryList,
+  name: r'fetchRepositoryListProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$fetchRepositoryListHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef FetchRepositoryListRef
+    = AutoDisposeFutureProviderRef<List<RepositoryListState>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
