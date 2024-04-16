a15fef52a0b85968a8bdbe9e6e8cffe7c008c8b1
Toma-0 <toma0122yukako@icloud.com>
2024-04-16 22:20:19 +0900

    リポジトリ詳細のState作成

---
+++ lib/model/repository_detail/repository_detail_state.freezed.dart
@@ -0,0 +1,382 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'repository_detail_state.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+RepositoryDetailState _$RepositoryDetailStateFromJson(
+    Map<String, dynamic> json) {
+  return _RepositoryDetailState.fromJson(json);
+}
+
+/// @nodoc
+mixin _$RepositoryDetailState {
+  int get id => throw _privateConstructorUsedError;
+  String get repositoryName => throw _privateConstructorUsedError;
+  String get ownerName => throw _privateConstructorUsedError;
+  String get ownerImageUri => throw _privateConstructorUsedError;
+  int get starCount => throw _privateConstructorUsedError;
+  int get watcherCount => throw _privateConstructorUsedError;
+  int get forksCount => throw _privateConstructorUsedError;
+  String get language => throw _privateConstructorUsedError;
+  String get description => throw _privateConstructorUsedError;
+  String get repositoryUrl => throw _privateConstructorUsedError;
+  String get upDateAt => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $RepositoryDetailStateCopyWith<RepositoryDetailState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $RepositoryDetailStateCopyWith<$Res> {
+  factory $RepositoryDetailStateCopyWith(RepositoryDetailState value,
+          $Res Function(RepositoryDetailState) then) =
+      _$RepositoryDetailStateCopyWithImpl<$Res, RepositoryDetailState>;
+  @useResult
+  $Res call(
+      {int id,
+      String repositoryName,
+      String ownerName,
+      String ownerImageUri,
+      int starCount,
+      int watcherCount,
+      int forksCount,
+      String language,
+      String description,
+      String repositoryUrl,
+      String upDateAt});
+}
+
+/// @nodoc
+class _$RepositoryDetailStateCopyWithImpl<$Res,
+        $Val extends RepositoryDetailState>
+    implements $RepositoryDetailStateCopyWith<$Res> {
+  _$RepositoryDetailStateCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? repositoryName = null,
+    Object? ownerName = null,
+    Object? ownerImageUri = null,
+    Object? starCount = null,
+    Object? watcherCount = null,
+    Object? forksCount = null,
+    Object? language = null,
+    Object? description = null,
+    Object? repositoryUrl = null,
+    Object? upDateAt = null,
+  }) {
+    return _then(_value.copyWith(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+      repositoryName: null == repositoryName
+          ? _value.repositoryName
+          : repositoryName // ignore: cast_nullable_to_non_nullable
+              as String,
+      ownerName: null == ownerName
+          ? _value.ownerName
+          : ownerName // ignore: cast_nullable_to_non_nullable
+              as String,
+      ownerImageUri: null == ownerImageUri
+          ? _value.ownerImageUri
+          : ownerImageUri // ignore: cast_nullable_to_non_nullable
+              as String,
+      starCount: null == starCount
+          ? _value.starCount
+          : starCount // ignore: cast_nullable_to_non_nullable
+              as int,
+      watcherCount: null == watcherCount
+          ? _value.watcherCount
+          : watcherCount // ignore: cast_nullable_to_non_nullable
+              as int,
+      forksCount: null == forksCount
+          ? _value.forksCount
+          : forksCount // ignore: cast_nullable_to_non_nullable
+              as int,
+      language: null == language
+          ? _value.language
+          : language // ignore: cast_nullable_to_non_nullable
+              as String,
+      description: null == description
+          ? _value.description
+          : description // ignore: cast_nullable_to_non_nullable
+              as String,
+      repositoryUrl: null == repositoryUrl
+          ? _value.repositoryUrl
+          : repositoryUrl // ignore: cast_nullable_to_non_nullable
+              as String,
+      upDateAt: null == upDateAt
+          ? _value.upDateAt
+          : upDateAt // ignore: cast_nullable_to_non_nullable
+              as String,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$RepositoryDetailStateImplCopyWith<$Res>
+    implements $RepositoryDetailStateCopyWith<$Res> {
+  factory _$$RepositoryDetailStateImplCopyWith(
+          _$RepositoryDetailStateImpl value,
+          $Res Function(_$RepositoryDetailStateImpl) then) =
+      __$$RepositoryDetailStateImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {int id,
+      String repositoryName,
+      String ownerName,
+      String ownerImageUri,
+      int starCount,
+      int watcherCount,
+      int forksCount,
+      String language,
+      String description,
+      String repositoryUrl,
+      String upDateAt});
+}
+
+/// @nodoc
+class __$$RepositoryDetailStateImplCopyWithImpl<$Res>
+    extends _$RepositoryDetailStateCopyWithImpl<$Res,
+        _$RepositoryDetailStateImpl>
+    implements _$$RepositoryDetailStateImplCopyWith<$Res> {
+  __$$RepositoryDetailStateImplCopyWithImpl(_$RepositoryDetailStateImpl _value,
+      $Res Function(_$RepositoryDetailStateImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? repositoryName = null,
+    Object? ownerName = null,
+    Object? ownerImageUri = null,
+    Object? starCount = null,
+    Object? watcherCount = null,
+    Object? forksCount = null,
+    Object? language = null,
+    Object? description = null,
+    Object? repositoryUrl = null,
+    Object? upDateAt = null,
+  }) {
+    return _then(_$RepositoryDetailStateImpl(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+      repositoryName: null == repositoryName
+          ? _value.repositoryName
+          : repositoryName // ignore: cast_nullable_to_non_nullable
+              as String,
+      ownerName: null == ownerName
+          ? _value.ownerName
+          : ownerName // ignore: cast_nullable_to_non_nullable
+              as String,
+      ownerImageUri: null == ownerImageUri
+          ? _value.ownerImageUri
+          : ownerImageUri // ignore: cast_nullable_to_non_nullable
+              as String,
+      starCount: null == starCount
+          ? _value.starCount
+          : starCount // ignore: cast_nullable_to_non_nullable
+              as int,
+      watcherCount: null == watcherCount
+          ? _value.watcherCount
+          : watcherCount // ignore: cast_nullable_to_non_nullable
+              as int,
+      forksCount: null == forksCount
+          ? _value.forksCount
+          : forksCount // ignore: cast_nullable_to_non_nullable
+              as int,
+      language: null == language
+          ? _value.language
+          : language // ignore: cast_nullable_to_non_nullable
+              as String,
+      description: null == description
+          ? _value.description
+          : description // ignore: cast_nullable_to_non_nullable
+              as String,
+      repositoryUrl: null == repositoryUrl
+          ? _value.repositoryUrl
+          : repositoryUrl // ignore: cast_nullable_to_non_nullable
+              as String,
+      upDateAt: null == upDateAt
+          ? _value.upDateAt
+          : upDateAt // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$RepositoryDetailStateImpl implements _RepositoryDetailState {
+  const _$RepositoryDetailStateImpl(
+      {required this.id,
+      required this.repositoryName,
+      required this.ownerName,
+      required this.ownerImageUri,
+      required this.starCount,
+      required this.watcherCount,
+      required this.forksCount,
+      required this.language,
+      required this.description,
+      required this.repositoryUrl,
+      required this.upDateAt});
+
+  factory _$RepositoryDetailStateImpl.fromJson(Map<String, dynamic> json) =>
+      _$$RepositoryDetailStateImplFromJson(json);
+
+  @override
+  final int id;
+  @override
+  final String repositoryName;
+  @override
+  final String ownerName;
+  @override
+  final String ownerImageUri;
+  @override
+  final int starCount;
+  @override
+  final int watcherCount;
+  @override
+  final int forksCount;
+  @override
+  final String language;
+  @override
+  final String description;
+  @override
+  final String repositoryUrl;
+  @override
+  final String upDateAt;
+
+  @override
+  String toString() {
+    return 'RepositoryDetailState(id: $id, repositoryName: $repositoryName, ownerName: $ownerName, ownerImageUri: $ownerImageUri, starCount: $starCount, watcherCount: $watcherCount, forksCount: $forksCount, language: $language, description: $description, repositoryUrl: $repositoryUrl, upDateAt: $upDateAt)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$RepositoryDetailStateImpl &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.repositoryName, repositoryName) ||
+                other.repositoryName == repositoryName) &&
+            (identical(other.ownerName, ownerName) ||
+                other.ownerName == ownerName) &&
+            (identical(other.ownerImageUri, ownerImageUri) ||
+                other.ownerImageUri == ownerImageUri) &&
+            (identical(other.starCount, starCount) ||
+                other.starCount == starCount) &&
+            (identical(other.watcherCount, watcherCount) ||
+                other.watcherCount == watcherCount) &&
+            (identical(other.forksCount, forksCount) ||
+                other.forksCount == forksCount) &&
+            (identical(other.language, language) ||
+                other.language == language) &&
+            (identical(other.description, description) ||
+                other.description == description) &&
+            (identical(other.repositoryUrl, repositoryUrl) ||
+                other.repositoryUrl == repositoryUrl) &&
+            (identical(other.upDateAt, upDateAt) ||
+                other.upDateAt == upDateAt));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      id,
+      repositoryName,
+      ownerName,
+      ownerImageUri,
+      starCount,
+      watcherCount,
+      forksCount,
+      language,
+      description,
+      repositoryUrl,
+      upDateAt);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$RepositoryDetailStateImplCopyWith<_$RepositoryDetailStateImpl>
+      get copyWith => __$$RepositoryDetailStateImplCopyWithImpl<
+          _$RepositoryDetailStateImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$RepositoryDetailStateImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _RepositoryDetailState implements RepositoryDetailState {
+  const factory _RepositoryDetailState(
+      {required final int id,
+      required final String repositoryName,
+      required final String ownerName,
+      required final String ownerImageUri,
+      required final int starCount,
+      required final int watcherCount,
+      required final int forksCount,
+      required final String language,
+      required final String description,
+      required final String repositoryUrl,
+      required final String upDateAt}) = _$RepositoryDetailStateImpl;
+
+  factory _RepositoryDetailState.fromJson(Map<String, dynamic> json) =
+      _$RepositoryDetailStateImpl.fromJson;
+
+  @override
+  int get id;
+  @override
+  String get repositoryName;
+  @override
+  String get ownerName;
+  @override
+  String get ownerImageUri;
+  @override
+  int get starCount;
+  @override
+  int get watcherCount;
+  @override
+  int get forksCount;
+  @override
+  String get language;
+  @override
+  String get description;
+  @override
+  String get repositoryUrl;
+  @override
+  String get upDateAt;
+  @override
+  @JsonKey(ignore: true)
+  _$$RepositoryDetailStateImplCopyWith<_$RepositoryDetailStateImpl>
+      get copyWith => throw _privateConstructorUsedError;
+}
---
+++ lib/model/repository_detail/repository_detail_state.g.dart
@@ -0,0 +1,39 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'repository_detail_state.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$RepositoryDetailStateImpl _$$RepositoryDetailStateImplFromJson(
+        Map<String, dynamic> json) =>
+    _$RepositoryDetailStateImpl(
+      id: json['id'] as int,
+      repositoryName: json['repositoryName'] as String,
+      ownerName: json['ownerName'] as String,
+      ownerImageUri: json['ownerImageUri'] as String,
+      starCount: json['starCount'] as int,
+      watcherCount: json['watcherCount'] as int,
+      forksCount: json['forksCount'] as int,
+      language: json['language'] as String,
+      description: json['description'] as String,
+      repositoryUrl: json['repositoryUrl'] as String,
+      upDateAt: json['upDateAt'] as String,
+    );
+
+Map<String, dynamic> _$$RepositoryDetailStateImplToJson(
+        _$RepositoryDetailStateImpl instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'repositoryName': instance.repositoryName,
+      'ownerName': instance.ownerName,
+      'ownerImageUri': instance.ownerImageUri,
+      'starCount': instance.starCount,
+      'watcherCount': instance.watcherCount,
+      'forksCount': instance.forksCount,
+      'language': instance.language,
+      'description': instance.description,
+      'repositoryUrl': instance.repositoryUrl,
+      'upDateAt': instance.upDateAt,
+    };
